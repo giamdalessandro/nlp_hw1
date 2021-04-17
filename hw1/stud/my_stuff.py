de8ba2fffe210b5aedd18e7e4c94fa06d9b1aa5d
zascerta <giammarco.daless@gmail.com>
2021-04-17 19:19:20 +0200

    Dataset class finished

--- hw1/stud/my_stuff.py
+++ hw1/stud/my_stuff.py
@@ -7,7 +7,7 @@ import collections
 
 #from tqdm import tqdm
 from typing import List, Tuple, Any, Dict
-from torch import Tensor, FloatTensor, relu, softmax
+from torch import Tensor, FloatTensor, LongTensor, relu, softmax
 from torch.nn import Embedding, Module, Linear, BCELoss
 from torch.utils.data import Dataset
 
@@ -29,31 +29,20 @@ class WordEmbDataset(Dataset):
             - window_size : Number of words to consider as context.
         """
         #self.window_size = window_size # [[w1,s1, w2,s1, ..., w|s1|,s1], ..., [w1,sn, ..., w|sn|,sn]] 
-        self.unk_token   = unk_token
-        self.sep_token   = sep_token
-        self.data_json   = self.read_dataset(data_path)  # tuple(s_pairs,labels)
-        self.build_vocabulary(vocab_size, unk_token, sep_token, merge=merge)
+        self.unk_token    = unk_token
+        self.sep_token    = sep_token
+        self.data_json    = self.__read_dataset(data_path)  # tuple(s_pairs,labels)
+        self.__build_vocabulary(vocab_size, unk_token, sep_token, merge=merge)
+        self.data_samples = self._preprocess_samples(unk_token, sep_token)
 
-    def merge_pair(self, spair: dict, sep_token: str):
-        """
-        Merge the sentences of a pair into one context, where the two are separated 
-        by the sep_token. # may include this in tokenize_line #RABARBARO
-        """
-        s1 = spair["sentence1"]
-        s2 = spair["sentence2"]
-        
-        s1.append(sep_token)
-        s1.extend(s2)
-        return s1
-
-    def tokenize_line(self, line: str, pattern='\W'):
+    def __tokenize_line(self, line: str, pattern='\W'):
         """
         Tokenizes a single line (e.g. "The pen is on the table" -> 
         ["the, "pen", "is", "on", "the", "table"]).
         """
         return [word.lower() for word in re.split(pattern, line.lower()) if word]
 
-    def read_dataset(self, data_path: str):
+    def __read_dataset(self, data_path: str):
         """
         Reads the dataset and converts each sentence in the input file into a list of tokenized words.
         """       
@@ -65,8 +54,8 @@ class WordEmbDataset(Dataset):
             for obj in f:
                 labels.append(obj.pop('label'))
 
-                obj["sentence1"] = self.tokenize_line(obj["sentence1"])
-                obj["sentence2"] = self.tokenize_line(obj["sentence2"])
+                obj["sentence1"] = self.__tokenize_line(obj["sentence1"])
+                obj["sentence2"] = self.__tokenize_line(obj["sentence2"])
                 sentence_pairs.append(obj)
 
         assert len(sentence_pairs) == len(labels)
@@ -76,11 +65,16 @@ class WordEmbDataset(Dataset):
         print("[INFO]: data loaded successfully.")
         return sentence_pairs, labels
 
-    # TODO
-    def build_vocabulary(self, vocab_size: int, unk_token: str, sep_token: str, merge: bool):
+    def __build_vocabulary(self, vocab_size: int, unk_token: str, sep_token: str, merge: bool):
         """ TODO
         Defines the vocabulary to be used. Builds a mapping (word, index) for
-        each word in the vocabulary.
+        each word in the vocabulary. It adds the following attributes to the class: 
+            self.distinct_words
+            self.word_to_idx
+            self.frequency
+            self.tot_occurrences 
+            self.id2word
+            self.data_samples
 
         Args:
             - vocab_size: size of the vocabolary;
@@ -91,17 +85,10 @@ class WordEmbDataset(Dataset):
         """
         print("\n[INFO]: building vocabulary ...")
         counter_list = []
-        # context is a list of tokens within a single sentence
         for spair in self.data_json[0]:
-            if merge:
-                context = self.merge_pair(spair=spair, sep_token=sep_token)
-                counter_list.extend(context)
-
-            else:
-                context1 = spair["sentence1"]
-                context2 = spair["sentence2"]
-                counter_list.extend(context1)
-                counter_list.extend(context2)
+            # context is a list of tokens within a single sentence
+            context = merge_pair(spair=spair, sep_token=sep_token, separate=merge)
+            counter_list.extend(context)
             
         counter = collections.Counter(counter_list)
         self.distinct_words = len(counter)
@@ -110,20 +97,17 @@ class WordEmbDataset(Dataset):
         # consider only the (vocab size -1) most common words to build the vocab
         most_common = enumerate(counter.most_common(vocab_size - 1))
         dictionary = {key: index for index, (key, _) in most_common}
-        # all the other words are mapped to UNK
+
         assert unk_token not in dictionary
         dictionary[unk_token] = vocab_size - 1
         if merge:
-            dictionary[unk_token] = vocab_size - 1
             dictionary[sep_token] = vocab_size
-
         self.word_to_idx = dictionary
 
         # dictionary with (word, frequency) pairs -- including only words that are in the vocabulary
         dict_counts = {x: counter[x] for x in dictionary if x is not unk_token}
         self.frequency = dict_counts
         self.tot_occurrences = sum(dict_counts[x] for x in dict_counts)
-
         print(f"Total occurrences of words in dictionary: {self.tot_occurrences}")
 
         less_freq_word = min(dict_counts, key=counter.get)
@@ -131,59 +115,75 @@ class WordEmbDataset(Dataset):
 
         # index to word dictonary
         self.id2word = {value: key for key, value in dictionary.items()}
-
-        # data is the text converted to indexes, as list of lists
-        data = []
-        # for each sentence
-        for spair in self.data_json[0]:
-            #sentence = self.merge_pair(spair=spair, sep_token=sep_token)
-            if merge:  #RABARBARO
-                sentence = self.merge_pair(spair=spair, sep_token=sep_token)
-            else:
-                sentence  = spair["sentence1"]
-                sentence2 = spair["sentence2"]
-                sentence.extend(sentence2)
-
-            paragraph = []
-            # for each word in the sentence
-            for i in sentence:
-                id_ = dictionary[i] if i in dictionary else dictionary[unk_token]
-                if id_ == dictionary[unk_token]:
-                    continue
-                paragraph.append(id_)
-            data.append(paragraph)
-        # list of lists of indices, where each sentence is a list of indices, ignoring UNK
-        self.data_idx = data
-
-class FooClassifier(Module):
-    """ TODO
-    Classifier class.
-    """
-    def __init__(self, input_features: int, hidden_size: int, output_classes: int):
-        super().__init__()
-        self.hidden_layer = Linear(input_features, hidden_size)
-        self.output_layer = Linear(hidden_size, output_classes)
-        self.loss_fn = BCELoss()
-        self.global_epoch = 0
-
-    def forward(self, x: Tensor, y: Tensor) -> Dict[str, Tensor]:
-        hidden_output = self.hidden_layer(x)
-        hidden_output = relu(hidden_output)
         
-        logits = self.output_layer(hidden_output).squeeze(1)
-        probabilities = softmax(logits, dim=-1)
-        result = {'logits': logits, 'probabilities': probabilities}
+    def _preprocess_samples(self, unk_token: str, sep_token: str):
+        """
+        Preprocess the data to create data samples. The samples are couples having 
+        a sentence pair associated with its label (e.g. <s_pair,label>).
+        """
+        # load pre-trained embeddings
+        pretrained_emb, _ = load_pretrained_embedding(pretrained_path, self.word_to_idx)
 
-        # compute loss
-        if y is not None:
-            loss = self.loss(logits, y)
-            result['loss'] = loss
-        return result
+        samples = []
+        emb_lookup = embedding_lookUp(pretrained_emb)
+        for spair, label in zip(self.data_json[0],self.data_json[1]):
+            paragraph = indexify(spair, self.word_to_idx, unk_token, sep_token)  
+            aux = emb_lookup(LongTensor(paragraph))      
+            sample = (aux, label)
+            samples.append(sample)
 
-    def loss(self, pred, y):
-        return self.loss_fn(pred, y)
+        return samples
 
-# TODO
+    # overrided method
+    def __len__(self):
+        """ Returns the number of samples in our dataset """
+        return len(self.data_samples)
+
+    def __getitem__(self, idx):
+        """ Returns the idx-th sample """
+        return self.data_samples[idx]
+
+#class FooClassifier(Module):
+#    """ TODO
+#    Classifier class.
+#    """
+#    def __init__(self, input_features: int, hidden_size: int, output_classes: int):
+#        super().__init__()
+#        self.hidden_layer = Linear(input_features, hidden_size)
+#        self.output_layer = Linear(hidden_size, output_classes)
+#        self.loss_fn = BCELoss()
+#        self.global_epoch = 0
+#
+#    def forward(self, x: Tensor, y: Tensor) -> Dict[str, Tensor]:
+#        hidden_output = self.hidden_layer(x)
+#        hidden_output = relu(hidden_output)
+#        
+#        logits = self.output_layer(hidden_output).squeeze(1)
+#        probabilities = softmax(logits, dim=-1)
+#        result = {'logits': logits, 'probabilities': probabilities}
+#
+#        # compute loss
+#        if y is not None:
+#            loss = self.loss(logits, y)
+#            result['loss'] = loss
+#        return result
+#
+#    def loss(self, pred, y):
+#        return self.loss_fn(pred, y)
+
+
+def merge_pair(spair: dict, sep_token: str, separate: bool):
+    """
+    Merge the sentences of a pair into one context, where the two are separated 
+    by the sep_token.
+    """
+    s1, s2 = spair["sentence1"], spair["sentence2"]
+    if separate:
+        s1.append(sep_token)
+    s1.extend(s2)
+    #print(s1)
+    return s1
+
 def load_pretrained_embedding(path: str, word_to_idx: dict):
     """
     Loads pre-trained word embeddings from 'path' file, and retrieves an 
@@ -238,15 +238,31 @@ def load_pretrained_embedding(path: str, word_to_idx: dict):
     print(f"Total of missing embeddings: {missing} ({round(missing/distinct_words,6)*100}% of vocabulary)" )
 
     embedding_mat = np.array(embedding_list, dtype=np.float64)
+    print("Total embeddings:",embedding_mat.shape)
     return embedding_mat, word_to_embedding
 
 def embedding_lookUp(pretrained_emb: np.ndarray):
+    """
+    Lookup table that matches a list of word indexes to their respective embedding tensors,
+    creating a pytorch embedding module.
+    """
     num_embeddings = pretrained_emb.shape[0]
     embedding_dim  = pretrained_emb.shape[1]
     return Embedding(num_embeddings, embedding_dim).from_pretrained(FloatTensor(pretrained_emb))
 
-def indexify():
-    return NotImplementedError
+def indexify(spair: dict, word_to_idx: dict, unk_token: str, sep_token: str):
+    """
+    Maps the words of the input sentences pair to the matching vocabulary indexes. 
+    """
+    indexes = []
+    merged = merge_pair(spair, sep_token, False)
+    for word in merged:
+        try:
+            indexes.append(word_to_idx[word])
+        except KeyError as e:
+            indexes.append(word_to_idx[unk_token])
+    
+    return indexes
 
 
 ######################### Main test #######################
@@ -268,13 +284,3 @@ if __name__ == '__main__':
     pretrained_path = os.path.join(PRETRAINED_DIR, "glove.6B", "glove.6B.50d.txt")
 
     dataset = WordEmbDataset(dev_data_path, VOCAB_SIZE, UNK, SEP, merge=False)
-    pretrained_emb, _ = load_pretrained_embedding(pretrained_path, dataset.word_to_idx)
-
-    # create pytorch embedding module
-    num_embeddings = pretrained_emb.shape[0]
-    embedding_dim  = pretrained_emb.shape[1]
-    embeddings = Embedding(num_embeddings, embedding_dim).from_pretrained(FloatTensor(pretrained_emb))
-    
-    print(type(pretrained_emb))
-    print(pretrained_emb.shape)
-    #print("Embedding example:", vocab_emb["cat"])
